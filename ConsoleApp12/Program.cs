7cbc358f63f4ace5caaf2bf85e1adf793fd2a781
DR4MM4 <153441126+DR4MM4@users.noreply.github.com>
2024-01-24 15:32:39 +0200

    Add files via upload

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //задание 1 y = -5x² + 2x +1
+            //for (int x=-5;x<=5;x+=2) 
+            //{           
+            //    Console.WriteLine($"x={x}\t y={-5*Math.Pow(x,2)+2*x+1}");
+            //}
+            //задание 2 
+            //for (double x = -3;x<=3.2;x+=0.2)
+            //{
+            //    Console.WriteLine($"x={x}\t y={Math.Abs(x)}");
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
